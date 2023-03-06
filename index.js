736f090f99c5740115bd5c4f76cc7276b72f5cc0
masterdude <xmertsahin@gmail.com>
2023-03-06 15:41:57 +0300

    Updated methods

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ let displayValue = "";
 
 let numbersArray = [];
 
-let num1 
+let num1;
 
-let num2
+let num2;
 
 let numberButtons = document.querySelectorAll(".button");
 
 let plusButton = document.querySelector(".plus");
+
+let minusButton = document.querySelector(".minus");
 
 let equalButton = document.querySelector(".equal");
 
@@ -20,71 +22,136 @@ let resultScreen = document.querySelector(".calculator-screen>h2");
 
 let isNumberOneEntered = false;
 
-let operatorFunction 
+let operatorFunction;
 
-
+let slicerCharacter;
 
 function add(number1, number2) {
-    return number1 + number2;
+  return number1 + number2;
 }
 
 function substract(number1, number2) {
-    return number1 - number2;
+  return number1 - number2;
 }
 
 function divide(number1, number2) {
-    return number1 / number2;
+  return number1 / number2;
 }
 
 function multiply(number1, number2) {
-    return number1 * number2;
+  return number1 * number2;
 }
 
-function operator (number1, number2, calculatingFunction){
-    return calculatingFunction(number1,number2);
+function operator(number1, number2, calculatingFunction) {
+  return calculatingFunction(number1, number2);
 }
-
-
 
 for (let index = 0; index < numberButtons.length; index++) {
-    numberButtons[index].addEventListener("click", function(event){
-        displayValue = displayValue + this.innerText;
-        displayScreen.innerText =  displayValue;
-    })
-    
+  numberButtons[index].addEventListener("click", function (event) {
+    displayValue = displayValue + this.innerText;
+    displayScreen.innerText = displayValue;
+  });
 }
-
 
 
 plusButton.addEventListener("click", function (params) {
-    operatorFunction = add;
-    if (num1 && num2 != true) {
-        console.log("am");
-        num2 = parseInt(displayValue.slice(displayValue.indexOf('+') + 1));
-        resultScreen.innerText = operator(num1, num2, operatorFunction);
-        num1 = parseInt(resultScreen.innerText);
-        num2 = 0;
-        displayValue = num1 + "+";
-        displayScreen.innerText = displayValue;
-        
-    } else{
-        num1 = parseInt(displayValue.replace("+", ""));
-        
+  switch (slicerCharacter) {
+    case "-":
+        num2 = parseInt(
+            displayValue.slice(displayValue.indexOf(slicerCharacter) + 1)
+          );
+          resultScreen.innerText = operator(num1, num2, operatorFunction);
+          num1 = parseInt(resultScreen.innerText);
+          num2 = null;
+          break;
+      break;
+
+    default:
+      break;
+  }
+  operatorFunction = add;
+  slicerCharacter = "+";
+  if (num1 && num2 != true) {
+    if (num2 === null) {
+      resultScreen.innerText = operator(num1, num2, operatorFunction);
+      num1 = parseInt(resultScreen.innerText);
+      num2 = NaN;
+      displayValue = num1 + slicerCharacter;
+      displayScreen.innerText = displayValue;
+    } else {
+      num2 = parseInt(
+        displayValue.slice(displayValue.indexOf(slicerCharacter) + 1)
+      );
+      resultScreen.innerText = operator(num1, num2, operatorFunction);
+      num1 = parseInt(resultScreen.innerText);
+      
+      displayValue = num1 + slicerCharacter;
+      displayScreen.innerText = displayValue;
     }
+  } else if (num1 === true && num2 === false){
+    console.log("aa");
+  }else {
+    num1 = parseInt(displayValue.replace(slicerCharacter, ""));
+  }
+});
 
-})
 
+
+minusButton.addEventListener("click", function (params) {
+  switch (slicerCharacter) {
+    case "+":
+      num2 = parseInt(
+        displayValue.slice(displayValue.indexOf(slicerCharacter) + 1)
+      );
+      resultScreen.innerText = operator(num1, num2, operatorFunction);
+      num1 = parseInt(resultScreen.innerText);
+      num2 = null;
+      break;
+
+    default:
+      break;
+  }
+
+  operatorFunction = substract;
+  slicerCharacter = "-";
+  if (num1 && num2 != true) {
+    if (num2 === null) {
+      resultScreen.innerText = operator(num1, num2, operatorFunction);
+      num1 = parseInt(resultScreen.innerText);
+      num2 = NaN;
+      displayValue = num1 + slicerCharacter;
+      displayScreen.innerText = displayValue;
+    } else {
+      num2 = parseInt(
+        displayValue.slice(displayValue.indexOf(slicerCharacter) + 1)
+      );
+      resultScreen.innerText = operator(num1, num2, operatorFunction);
+      num1 = parseInt(resultScreen.innerText);
+      
+      displayValue = num1 + slicerCharacter;
+      displayScreen.innerText = displayValue;
+    }
+  } else if (num1 === true && num2 === false){
+    console.log("aa");
+  }else {
+    num1 = parseInt(displayValue.replace(slicerCharacter, ""));
+  }
+});
 
 equalButton.addEventListener("click", function (params) {
-    
-    num2 = parseInt(displayValue.slice(displayValue.indexOf('+') + 1));
-    resultScreen.innerText = operator(num1, num2, operatorFunction);
-})
+  num2 = parseInt(
+    displayValue.slice(displayValue.indexOf(slicerCharacter) + 1)
+  );
+  resultScreen.innerText = operator(num1, num2, operatorFunction);
+  displayScreen.innerText = resultScreen.innerText;
+});
+
+function equal() {}
 
 clearButton.addEventListener("click", function (params) {
-    num1 = 0;
-    num2 = 0;
-    displayValue = "";
-    displayScreen.innerText = "";
-    resultScreen.innerText = "Cleared";
-})
+  num1 = null;
+  num2 = null;
+  displayValue = "";
+  displayScreen.innerText = "";
+  resultScreen.innerText = "Cleared";
+});
