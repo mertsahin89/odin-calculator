6669592a049c486864403d79a5c35dba0453cf7a
masterdude <xmertsahin@gmail.com>
2023-03-11 12:56:28 +0300

    Added operator checker when screen is empty

--- index.js
+++ index.js
@@ -50,14 +50,62 @@ function operator(number1, number2, calculatingFunction) {
   return calculatingFunction(number1, number2);
 }
 
+
+function emptyScreenOperatorHandle(){
+  if (displayValue === "+" ||
+  displayValue === "-" ||
+  displayValue === "*" || 
+  displayValue === "=" ||
+  displayValue === "/") {
+    alert("No number input dedected");
+    displayScreen.innerText = "";
+    displayValue = "";
+    window.classList.remove("pressed");
+  }
+}
+
+function operatorButtonToggle(pressedButton, op1, op2, op3, op4, noneOrAutoString ) {
+  pressedButton.classList.remove("pressed");
+  pressedButton.style.pointerEvents = noneOrAutoString;
+  op1.style.pointerEvents = noneOrAutoString;
+  op2.style.pointerEvents = noneOrAutoString;
+  op3.style.pointerEvents = noneOrAutoString;
+  op4.style.pointerEvents = noneOrAutoString;
+}
+
+
 for (let index = 0; index < numberButtons.length; index++) {
-  numberButtons[index].addEventListener("click", function (event) {
+  numberButtons[index].addEventListener("mousedown", function () {
     displayValue = displayValue + this.innerText;
     displayScreen.innerText = displayValue;
+    this.classList.add("pressed");
+    let checkLastChar = this.innerText;
+    console.log(checkLastChar);
+
+    if (checkLastChar !== "+" || checkLastChar !== "-" || checkLastChar !== "*" || checkLastChar !== "/") {
+      let pressedItems = document.querySelectorAll(".pressed");
+      let buttonItems = [plusButton, minusButton, multiplyButton, divideButton, equalButton];
+      pressedItems.forEach(element => {
+        element.classList.remove("pressed");
+      });
+      
+      buttonItems.forEach(element => {
+        element.style.pointerEvents = "auto"
+      });
+    }
+  });
+
+  numberButtons[index].addEventListener("mouseup", function (params) {
+    this.classList.remove("pressed");
   });
 }
 
-plusButton.addEventListener("click", function (params) {
+
+
+plusButton.addEventListener("click", function () {
+  operatorButtonToggle(this, minusButton, divideButton, multiplyButton, equalButton, "none");
+  emptyScreenOperatorHandle();
+  this.classList.add("pressed");
   switch (slicerCharacter) {
     case "-":
       num2 = parseInt(
@@ -89,9 +137,12 @@ plusButton.addEventListener("click", function (params) {
     default:
       break;
   }
+
+  
+
   operatorFunction = add;
   slicerCharacter = "+";
-  if (num1 && num2 != true) {
+  if (num1 && num2 !== true) {
     if (num2 === null) {
       resultScreen.innerText = operator(num1, num2, operatorFunction);
       num1 = parseInt(resultScreen.innerText);
@@ -116,6 +167,9 @@ plusButton.addEventListener("click", function (params) {
 });
 
 minusButton.addEventListener("click", function (params) {
+  operatorButtonToggle(this, plusButton, divideButton, multiplyButton, equalButton, "none");
+  emptyScreenOperatorHandle();
+  this.classList.add("pressed");
   switch (slicerCharacter) {
     case "+":
       num2 = parseInt(
@@ -175,6 +229,9 @@ minusButton.addEventListener("click", function (params) {
 });
 
 multiplyButton.addEventListener("click", function () {
+  operatorButtonToggle(this, minusButton, divideButton, plusButton, equalButton, "none");
+  emptyScreenOperatorHandle();
+  this.classList.add("pressed");
   switch (slicerCharacter) {
     case "-":
       num2 = parseInt(
@@ -234,6 +291,9 @@ multiplyButton.addEventListener("click", function () {
 });
 
 divideButton.addEventListener("click", function name(params) {
+  operatorButtonToggle(this, minusButton, plusButton, multiplyButton, equalButton, "none");
+  emptyScreenOperatorHandle();
+  this.classList.add("pressed");
   switch (slicerCharacter) {
     case "-":
       num2 = parseInt(
@@ -293,6 +353,7 @@ divideButton.addEventListener("click", function name(params) {
 });
 
 equalButton.addEventListener("click", function (params) {
+  emptyScreenOperatorHandle();
   num2 = parseInt(
     displayValue.slice(displayValue.indexOf(slicerCharacter) + 1)
   );
--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
-Calculator
+# Calculator Project Made With Html+Css+Js
+
+[Live](https://mertsahin89.github.io/odin-calculator/) 
+
+I know there are some bugs in this project but i'm planning to get back to refactoring in the future.
